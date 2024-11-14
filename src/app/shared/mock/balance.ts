2bedb399da02908d43a52e8f63243ea604ca9321
Asror Shodiyev <asrorshodiyev@Asror-2.local>
2024-11-14 18:29:29 +0500

    all

---
+++ src/app/shared/mock/balance.ts
@@ -0,0 +1,44 @@
+export const balances = [
+	{
+		"id": 1,
+		"short_name": "POK",
+		"name": "ПокерОК",
+		"code": "USD",
+		"amount": 0
+	},
+	{
+		"id": 15,
+		"short_name": "LUX",
+		"name": "Luxon",
+		"code": "USD",
+		"amount": 0
+	},
+	{
+		"id": 8,
+		"short_name": "GG",
+		"name": "GGPoker",
+		"code": "USD",
+		"amount": 0
+	},
+	{
+		"id": 12,
+		"short_name": "ACR",
+		"name": "ACR Poker",
+		"code": "USD",
+		"amount": 0
+	},
+	{
+		"id": 10,
+		"short_name": "PK",
+		"name": "PokerKing",
+		"code": "USD",
+		"amount": 0
+	},
+	{
+		"id": 11,
+		"short_name": "RS",
+		"name": "RedStar",
+		"code": "USD",
+		"amount": 0
+	}
+];
